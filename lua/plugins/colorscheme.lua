8fd319a54e913f147766e77ef14f921341e06dfd
Chris <chrisoliver345@gmail.com>
2025-06-18 18:27:13 +1200

    Fixed auto completion settings

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -7,5 +7,22 @@ return {
     config = function()
       vim.cmd.colorscheme("tokyonight")
     end
-  }
+  },
+  {
+    "shaunsingh/nord.nvim",
+    lazy = false,
+    name = "nord",
+    priority = 10000,
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = false,
+    name = "kanagawa",
+    priority = 10000,
+    config = function()
+      require("kanagawa").setup({
+        transparent = true,
+      })
+    end
+  },
 }
